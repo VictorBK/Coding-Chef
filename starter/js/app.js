412a830464376180241be7a16590916ea3fa1173
VictorBK <victorbrian4@gmail.com>
2023-02-15 16:51:19 +0300

    Navigation bar styling and toggling

--- starter/js/app.js
+++ starter/js/app.js
@@ -1,9 +1,8 @@
 const getElement = (selector) => {
   const element = document.querySelector(selector)
-
   if (element) return element
   throw Error(
-    `Please double check your class names, there is no ${selector} class`
+    `Please counter check your class names, there is no ${selector} class`
   )
 }
 
@@ -13,7 +12,3 @@ const navBtnDOM = getElement('.nav-btn')
 navBtnDOM.addEventListener('click', () => {
   links.classList.toggle('show-links')
 })
-
-const date = getElement('#date')
-const currentYear = new Date().getFullYear()
-date.textContent = currentYear
